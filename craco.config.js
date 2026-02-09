44a293b1c326c32229a58e3b2b1cfe665f08f881
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-02-09 19:42:56 +0400

    fix: simplify webpack alias configuration by removing redundant paths

--- craco.config.js
+++ craco.config.js
@@ -3,14 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@/components': path.resolve(__dirname, 'src/components'),
-      '@/hooks': path.resolve(__dirname, 'src/hooks'),
-      '@/utils': path.resolve(__dirname, 'src/utils'),
-      '@/contexts': path.resolve(__dirname, 'src/contexts'),
-      '@/pages': path.resolve(__dirname, 'src/pages'),
-      '@/routes': path.resolve(__dirname, 'src/routes'),
-      '@/constants': path.resolve(__dirname, 'src/constants')
+      '@': path.resolve(__dirname, 'src')
     }
   }
 };
