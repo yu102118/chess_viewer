580f0d62796dcdde3d3a6baaa8ed2809b1d39e13
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-02-10 22:14:08 +0400

    feat: update index exports and add settings subpage for theme and export customization

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export { default as AboutPage } from './AboutPage';
 export { default as DownloadPage } from './DownloadPage';
 export { default as SupportPage } from './SupportPage';
 export { default as NotFoundPage } from './NotFoundPage';
-export { default as ThemeCustomizerPage } from './ThemeCustomizerPage';
+export { default as SettingsPage } from './SettingsPage';
 export { default as FENHistoryPage } from './FENHistoryPage';
 export { default as AdvancedFENInputPage } from './AdvancedFENInputPage';
---
+++ src/pages/settings/index.js
@@ -0,0 +1,2 @@
+export { default as ThemeCustomization } from './ThemeCustomization';
+export { default as ExportCustomization } from './ExportCustomization';
