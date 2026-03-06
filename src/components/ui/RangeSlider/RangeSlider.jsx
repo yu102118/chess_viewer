ad8bf5c28debb4559174460e790b45218476200c
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-06 23:59:07 +0400

    refactor: enhance documentation for RangeSlider component

--- src/components/ui/RangeSlider/RangeSlider.jsx
+++ src/components/ui/RangeSlider/RangeSlider.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+/**
+ * Styled range slider wrapping a native `<input type="range">`.
+ * @param {Object} props
+ * @param {number} props.min - Minimum value
+ * @param {number} props.max - Maximum value
+ * @param {number} [props.step] - Step increment
+ * @param {number} props.value - Controlled value
+ * @param {Function} props.onChange - Change handler
+ * @param {string} [props.className=''] - Additional Tailwind classes
+ * @returns {JSX.Element}
+ */
 const RangeSlider = React.memo(
   ({ min, max, step, value, onChange, className = '' }) => {
     return (
