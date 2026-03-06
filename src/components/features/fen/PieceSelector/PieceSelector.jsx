721fcff04fb8e28af4b0263c008cd4830d0809e7
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-06 23:55:07 +0400

    refactor: enhance documentation for PieceSelector component

--- src/components/features/fen/PieceSelector/PieceSelector.jsx
+++ src/components/features/fen/PieceSelector/PieceSelector.jsx
@@ -1,6 +1,13 @@
 import { SearchableSelect } from '@/components/ui';
 import { PIECE_SETS } from '@/constants';
 
+/**
+ * Searchable dropdown for selecting the active chess piece style.
+ * @param {Object} props
+ * @param {string} props.pieceStyle - Currently selected piece style identifier
+ * @param {Function} props.setPieceStyle - Updates the active piece style
+ * @returns {JSX.Element}
+ */
 const PieceSelector = ({ pieceStyle, setPieceStyle }) => {
   return (
     <SearchableSelect
