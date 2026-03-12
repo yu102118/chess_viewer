ffaa114452141798d6aaed49298b01c4aa60b396
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-12 21:55:46 +0400

    feat: add theme customization constants for improved styling options

---
+++ src/constants/themeCustomization.js
@@ -0,0 +1,20 @@
+export const MAX_TOTAL_PRESETS = 48;
+export const STANDARD_PRESETS_COUNT = 19;
+export const RANK_GUTTER = 20;
+export const BOARD_SIZE_EXPR = 'min(52vh, 46vw)';
+export const CELL_SIZE_EXPR = `calc(${BOARD_SIZE_EXPR} / 8)`;
+export const STORAGE_KEYS = {
+  PRESETS: 'chess-diagram-presets',
+  SETTINGS: 'chess-diagram-settings',
+  LIGHT_SQUARE: 'chess-light-square',
+  DARK_SQUARE: 'chess-dark-square'
+};
+export const WOOD_PRESET = {
+  id: 'wood-default',
+  name: 'Wood',
+  light: '#d4af7a',
+  dark: '#8b4513',
+  isDefault: true,
+  isDeletable: false,
+  isEditable: false
+};
