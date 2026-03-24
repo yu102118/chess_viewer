3d73dbb5c75259ff8e0662454c3332b537713a05
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-24 21:01:47 +0400

    refactor: remove unused passive event listener detection code

--- src/utils/eventUtils.js
+++ src/utils/eventUtils.js
@@ -1,30 +1,4 @@
 export { throttle, debounce, rafThrottle } from './performance';
 
+/** @type {{ passive: true }} */
 export const passiveEventOptions = { passive: true };
-
-/**
- * Detects browser support for passive event listeners.
- *
- * @returns {boolean} True if passive events are supported
- */
-function detectPassiveSupport() {
-  let supported = false;
-
-  try {
-    const testOptions = {
-      get passive() {
-        supported = true;
-        return false;
-      }
-    };
-
-    window.addEventListener('test', null, testOptions);
-    window.removeEventListener('test', null, testOptions);
-  } catch {
-    supported = false;
-  }
-
-  return supported;
-}
-
-export const supportsPassive = detectPassiveSupport();
